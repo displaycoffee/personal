afe887739f1701b77eef8b5a940e8740a61001be
Adria Murphy <adria.m.murphy@gmail.com>
2016-03-05 14:57:36 -0700

    Add variables for paths, css task, and sass task

--- wordpress/gulpfile.js
+++ wordpress/gulpfile.js
@@ -5,52 +5,63 @@ var browserSync = require('browser-sync').create(),
 	gulp = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),
 	concat = require('gulp-concat'),
-	cssnano = require('gulp-cssnano'),
 	sass = require('gulp-sass'),
 	uglify = require('gulp-uglify');
 
-/* Variables
+/* Global Variables
    ---------------------------------------------- */
 
 var wpFolder = 'plugins';
-var folderName = 'custom-stuff';
-var dev = 'dev/' + wpFolder + '/' + folderName;
-var dist = 'wp-content/' + wpFolder + '/' + folderName;
+var dirName = 'custom-stuff';
 var proxyURL = 'http://localhost/personal/wordpress';
+
+/* Development Variables
+   ---------------------------------------------- */
+
+var dev = 'dev/' + wpFolder + '/' + dirName;
+var devJS = dev + '/assets/js';
+var devSass = dev + '/assets/scss';
+
+/* Distribution Variables
+   ---------------------------------------------- */
+
+var dist = 'wp-content/' + wpFolder + '/' + dirName;
+var distJS = dist + '/assets/js';
+var distCSS = dist + '/assets/css';
 
 /* JavaScript
    ---------------------------------------------- */
 
 var jsSources = [
-	dev + '/assets/js/media-library.js',
-	dev + '/assets/js/image-reset.js',
-	dev + '/assets/js/run-functions.js'
+	devJS + '/media-library.js',
+	devJS + '/image-reset.js',
+	devJS + '/run-functions.js'
 ];
 
 gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('functions.js'))
-		.pipe(gulp.dest(dev + '/assets/js'))
+		.pipe(gulp.dest(devJS))
 		.pipe(uglify())
-		.pipe(gulp.dest(dist + '/assets/js'));
+		.pipe(gulp.dest(distJS));
 });
 
 /* SASS
    ---------------------------------------------- */
 
-var sassSources = [dev + '/assets/scss/styles.scss'];
+var sassSources = [devSass + '/styles.scss'];
 
 gulp.task('sass', function() {
 	gulp.src(sassSources)
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(autoprefixer())
-		.pipe(gulp.dest(dist + '/assets/css'));
+		.pipe(gulp.dest(distCSS));
 });
 
 /* CSS
    ---------------------------------------------- */
 
-var cssSources = [dist + '/assets/css/styles.css'];	
+var cssSources = [distCSS + '/styles.css'];	
 
 gulp.task('css', function() {
 	gulp.src(cssSources)
@@ -76,7 +87,7 @@ gulp.task('watch', function() {
         open: false
     })
 	gulp.watch(jsSources, ['js']);
-	gulp.watch(dev + '/assets/scss/*.scss', ['sass']);
+	gulp.watch(devSass + '/*.scss', ['sass']);
 	gulp.watch(cssSources, ['css']);
 	gulp.watch(staticSources, ['static']);
 });
@@ -84,4 +95,4 @@ gulp.task('watch', function() {
 /* Default Gulp Task
    ---------------------------------------------- */
 
-gulp.task('default', ['js', 'static', 'watch']);
+gulp.task('default', ['js', 'sass', 'css', 'static', 'watch']);
