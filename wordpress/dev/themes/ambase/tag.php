b54f43e6cbbdf7c5d2a83e1655fbf92d7d75acdd
Adria Murphy <adria.m.murphy@gmail.com>
2016-03-06 19:06:45 -0700

    Update formatting, remove elements, change elements.

--- wordpress/dev/themes/ambase/tag.php
+++ wordpress/dev/themes/ambase/tag.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<header class="header">
-<h1 class="entry-title"><?php _e( 'Tag Archives: ', 'ambase' ); ?><?php single_tag_title(); ?></h1>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-</section>
+<div id="content" role="main">
+	<h1 class="entry-title">
+		<?php _e( 'Tag Archives: ', 'ambase' ); ?><?php single_tag_title(); ?>
+	</h1>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<?php get_template_part( 'entry' ); ?>
+	<?php endwhile; endif; ?>
+	<?php get_template_part( 'nav', 'below' ); ?>
+</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
