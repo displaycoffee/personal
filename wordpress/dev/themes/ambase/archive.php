572056fe505502dc9f5d33c217b4bceab633ac36
Adria Murphy <adria.m.murphy@gmail.com>
2016-04-07 20:54:49 -0600

    Update archive title

--- wordpress/dev/themes/ambase/archive.php
+++ wordpress/dev/themes/ambase/archive.php
@@ -8,9 +8,7 @@
 	get_header(); 
 ?>
 <article>
-	<header class="main-title">
-		<?php the_archive_title( '<h1>', '</h1>' ); ?>
-	</header>
+	<?php the_archive_title( '<header class="main-title"><h1>', '</h1></header>' ); ?>
 	<?php the_archive_description( '<div class="category-description">', '</div>' ); ?>	
 	<h2><?php _e( 'Posts', 'ambase' ); ?></h2>
 	<?php get_template_part( 'loop', 'index' ); ?>
