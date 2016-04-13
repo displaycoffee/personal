fdb42fec5cf6ffa1f8797154f1d09c5abfb00571
Adria Murphy <adria.m.murphy@gmail.com>
2016-04-12 21:35:54 -0600

    Add theme support for html5

--- wordpress/wp-content/themes/ambase/functions.php
+++ wordpress/wp-content/themes/ambase/functions.php
@@ -8,6 +8,13 @@
 		add_theme_support( 'title-tag' );
 		add_theme_support( 'automatic-feed-links' );
 		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );	
 
 		// Register navigation menus
 		register_nav_menus( array(
