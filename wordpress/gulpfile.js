5f9b3ccefe33a683b695bbc59a7a97e2449c2632
Adria Murphy <adria.m.murphy@gmail.com>
2016-04-28 18:11:47 -0600

    Fix spacing

--- wordpress/gulpfile.js
+++ wordpress/gulpfile.js
@@ -11,8 +11,10 @@ var browserSync = require('browser-sync').create(),
 /* Global Variables
    ---------------------------------------------- */
 
-var wpFolder = 'themes';
-var dirName = 'ambase';
+// var wpFolder = 'themes';
+// var dirName = 'ambase';
+var wpFolder = 'plugins';
+var dirName = 'custom-stuff';
 var proxyURL = 'http://localhost/personal/wordpress';
 
 /* Development Variables
@@ -39,18 +41,17 @@ if (wpFolder == 'themes') {
    ---------------------------------------------- */
 
 // ambase js
-var jsSources = [
-	devJS + '/navigation-links.js',
-	devJS + '/run-functions.js'
-];
-
-// custom-stuff js
-// var jsSources = [	
-// 	devJS + '/image-reset.js',
-// 	devJS + '/media-library.js',
+// var jsSources = [
+// 	devJS + '/navigation-links.js',
 // 	devJS + '/run-functions.js'
 // ];
 
+// custom-stuff js
+var jsSources = [	
+	devJS + '/image-reset.js',
+	devJS + '/media-library.js',
+	devJS + '/run-functions.js'
+];
 
 gulp.task('js', function() {
 	gulp.src(jsSources)
