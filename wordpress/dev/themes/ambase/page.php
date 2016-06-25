fa37b678ad483243897a88807b691de327a23a6a
displaycoffee <adria.m.murphy@gmail.com>
2016-06-25 13:45:22 -0600

    Move main-title above content section tag

--- wordpress/dev/themes/ambase/page.php
+++ wordpress/dev/themes/ambase/page.php
@@ -9,17 +9,21 @@
 	// Include header	
 	get_header(); 
 ?>
-<article>
-	<?php the_title( '<header class="main-title"><h1>', '</h1></header>' ); ?>	
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<div class="entry-single">	
-			<div id="entry-<?php esc_attr( the_ID() ); ?>" class="entry page">
-				<?php get_template_part( 'partials/entry', 'thumbnail' ); ?>
-				<div class="entry-content"><?php the_content(); ?></div>
-				<?php edit_post_link( __('Edit', 'ambase'), '<footer class="entry-footer"><div class="edit">', '</div></footer>' ); ?>				
-			</div>
-		</div>
-	<?php endwhile; endif; ?>
-</article>
-<?php get_sidebar(); ?>
+<?php the_title( '<header class="main-title"><div class="wrapper"><h1>', '</h1></div></header>' ); ?>
+<section class="content">
+	<div class="wrapper">
+		<article>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<div class="entry-single">	
+					<div id="entry-<?php esc_attr( the_ID() ); ?>" class="entry page">
+						<?php get_template_part( 'partials/entry', 'thumbnail' ); ?>
+						<div class="entry-content"><?php the_content(); ?></div>
+						<?php edit_post_link( __('Edit', 'ambase'), '<footer class="entry-footer"><div class="edit">', '</div></footer>' ); ?>				
+					</div>
+				</div>
+			<?php endwhile; endif; ?>
+		</article>
+		<?php get_sidebar(); ?>
+	</div>
+</section>			
 <?php get_footer(); ?>
