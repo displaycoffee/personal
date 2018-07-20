de85f3da67a85b6faa30d37d4fa4434b1490c294
displaycoffee <adria.m.murphy@gmail.com>
2018-07-20 17:52:00 -0600

    Initial commit of codes

---
+++ codes/README.md
@@ -0,0 +1,7 @@
+Codes
+===========================
+
+I'll be using this directory to add free-for-use template codes which can be copy and pasted on sites. More information to come later.
+
+**Updates:**
+* Meh
---
+++ codes/dev/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Page Title</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+    </head>
+    <body>
+        <!--[if lt IE 9]>
+            <p class="browserupgrade">
+                You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.
+            </p>
+        <![endif]-->
+        <h1>This is header 1.</h1>
+        <h2>This is header 2.</h2>
+        <h3>This is header 3.</h3>
+        <h4>This is header 4.</h4>
+        <h5>This is header 5.</h5>
+        <h6>This is header 6.</h6>
+        <p>This is a paragraph.</p>
+        <blockquote>This is a blockquote.</blockquote>
+        <ol>
+            <li>This is...</li>
+            <li>a...</li>
+            <li>ordered list.</li>
+        </ol>
+        <ul>
+            <li>This is...</li>
+            <li>an...</li>
+            <li>unordered list.</li>
+        </ul>
+        <dl>
+          <dt><dfn>Word 1</dfn></dt>
+          <dd>Defintion 1</dd>
+          <dt><dfn>Word 2</dfn></dt>
+          <dd>Defintion 2</dd>
+        </dl>
+        <main>This is a main tag.</main>
+        <a href="http://www.google.com">What about a link?</a>
+        <strong>This is a strong tag.</strong>
+        <em>And here's some ems.</em>
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
