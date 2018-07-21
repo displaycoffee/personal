f935daf858e268cd4b2d7e8977146eb7175019f8
displaycoffee <adria.m.murphy@gmail.com>
2018-07-20 22:32:50 -0600

    Finish making template responsive

--- codes/dist/includes/fnf.php
+++ codes/dist/includes/fnf.php
@@ -1,51 +1,54 @@
-<div class="shark-thing">
-	   <div class="hover-button">Hover For More</div>
-	   <div class="wrapper">
-		   <div class="information">
-			   <div class="top-bar">
-				   <div class="name">Imma Shark, Baby</div>
-				   <div class="species"><strong>Species:</strong> Shark, duh.</div>
-				   <div class="avatar"><img src="http://i.imgur.com/N5Ddvyv.gif" /></div>
-			   </div>
-			   <div class="stats-and-stuff">
-				   <div class="title">Best Shark In the Ocean!</div>
-				   <div class="blocks">
-					   <div class="half">
-						   482<span>Posts</span>
-					   </div>
-					   <div class="half">
-						   25<span>Topics</span>
-					   </div>
-					   <div class="half">
-						   22 years<span>Age</span>
-					   </div>
-					   <div class="half">
-						   Male<span>Gender</span>
-					   </div>
-					   <div class="half">
-						   Blood<span>Likes</span>
-					   </div>
-					   <div class="half">
-						   Being on land<span>Dislikes</span>
-					   </div>
-				   </div>
-				   <div class="quote">
-					   I am a nice shark, not a mindless eating machine. If I am to change this image, I must first change myself. Fish are friends, not food.
-					   <span>- Bruce</span>
-				   </div>
+<div class="fnf">
+   <div class="fnf-wrapper">
+	   <div class="fnf-hover-button">Hover For More</div>
+	   <div class="fnf-information">
+		   <div class="fnf-top-bar">
+			   <div class="fnf-name">Imma Shark, Baby</div>
+			   <div class="fnf-species"><strong>Species:</strong> Shark, duh.</div>
+			   <div class="fnf-avatar">
+				   <img src="http://i.imgur.com/N5Ddvyv.gif" />
 			   </div>
 		   </div>
-		   <div class="more-information">
-			   <div class="blocks">
-				   Quagga mussel cat shark is batfish frogfish. . Peppered moray clownfish hawkfish at seahorse hammerhead a funny snake eel. Wobbegong shark blue whale gold damsel faucet snail, in snake eel sea coral grouper. The crackin crazy Neon goby nurse shark moon. Quagga musse.
-				   <br /><br />
-				   Grey whale mermaid appeared. Fire fish pipefish blue tang, frogfish bicolor blenny at, quagga mussel and funny bannerfish. Milk shark swim Asian clam soldierfish. Banana wrasse in yellow pseudochromis non mermaid parrotfish mystery snail. In seahorse banded eel, butter hamlet are butterflyfish blue tang, wabas.
+		   <div class="fnf-profile">
+			   <div class="fnf-title fnf-block">Best Shark In the Ocean!</div>
+			   <div class="fnf-grid">
+				   <div class="fnf-column fnf-block">
+					   482<span>Posts</span>
+				   </div>
+				   <div class="fnf-column fnf-block">
+					   25<span>Topics</span>
+				   </div>
+				   <div class="fnf-column fnf-block">
+					   22 years<span>Age</span>
+				   </div>
+				   <div class="fnf-column fnf-block">
+					   Male<span>Gender</span>
+				   </div>
+				   <div class="fnf-column fnf-block">
+					   Blood<span>Likes</span>
+				   </div>
+				   <div class="fnf-column fnf-block">
+					   Being on land<span>Dislikes</span>
+				   </div>
 			   </div>
-			   <div class="blocks">
-				   Pigtoe sea coral king crab heelsplitter, in painted comber Banded eel bicolor blenny. Bursa trigger funny filefish, a Snaggletooth shark lobster in. Spotted sweetlips loving in catshark pebblesnail at seabass with faucet snail. Yellow pseudochromis weasel shark shadow, darkness in deap ocean an, Motionless Wabash pigtoe Full moon. Fishies asian cla.
-				   <br /><br />
-				   Painted comber, king crab a elktoe a, grey whale seahorse. The crackin crazy Neon goby nurse shark moon. Hippo tang gold damsel peaclam.
+			   <div class="fnf-quote fnf-block">
+				   I am a nice shark, not a mindless eating machine. If I am to change this image, I must first change myself. Fish are friends, not food.
+				   <span>- Bruce</span>
 			   </div>
 		   </div>
 	   </div>
+	   <div class="fnf-more">
+		   <div class="fnf-block">
+			   Quagga mussel cat shark is batfish frogfish. . Peppered moray clownfish hawkfish at seahorse hammerhead a funny snake eel. Wobbegong shark blue whale gold damsel faucet snail, in snake eel sea coral grouper. The crackin crazy Neon goby nurse shark moon. Quagga musse.
+			   <br /><br />
+			   Grey whale mermaid appeared. Fire fish pipefish blue tang, frogfish bicolor blenny at, quagga mussel and funny bannerfish. Milk shark swim Asian clam soldierfish. Banana wrasse in yellow pseudochromis non mermaid parrotfish mystery snail. In seahorse banded eel, butter hamlet are butterflyfish blue tang, wabas.
+		   </div>
+		   <div class="fnf-divider"></div>
+		   <div class="fnf-block">
+			   Pigtoe sea coral king crab heelsplitter, in painted comber Banded eel bicolor blenny. Bursa trigger funny filefish, a Snaggletooth shark lobster in. Spotted sweetlips loving in catshark pebblesnail at seabass with faucet snail. Yellow pseudochromis weasel shark shadow, darkness in deap ocean an, Motionless Wabash pigtoe Full moon. Fishies asian cla.
+			   <br /><br />
+			   Painted comber, king crab a elktoe a, grey whale seahorse. The crackin crazy Neon goby nurse shark moon. Hippo tang gold damsel peaclam.
+		   </div>
+	   </div>
    </div>
+</div>
