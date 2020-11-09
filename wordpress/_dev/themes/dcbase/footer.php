b8c6a82de31b4a123461a25e0850f2fb6263e48c
displaycoffee <adria.m.murphy@gmail.com>
2020-11-08 18:36:26 -0700

    Work on styles and base layout

--- wordpress/_dev/themes/dcbase/footer.php
+++ wordpress/_dev/themes/dcbase/footer.php
@@ -1,10 +1,13 @@
-</div>
-<footer id="footer">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'dcbase' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div>
-</footer>
-</div>
-<?php wp_footer(); ?>
+				</section>
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
+	</main>
+	<footer id="footer">
+		<div id="copyright">
+			&copy; <?php echo esc_html( date_i18n( __( 'Y', 'dcbase' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+		</div>
+	</footer>
+	<?php wp_footer(); ?>
 </body>
 </html>
